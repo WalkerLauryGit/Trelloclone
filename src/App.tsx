1aa068b0b1fa533bd8b9b922d162f919159935d1
Walker Laury <walkerlaury@wlmpb.local>
2021-10-06 15:35:16 -0500

    Getting everything together

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import {FC} from 'react'
+import {Column} from './Column'
+import {Card} from './Card'
+import {AppContainer} from './styles';
+import { AddNewItem } from './AddNewItem';
 
-function App() {
+export const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppContainer>
+      <Column text="To Do">
+        <Card text="Generate App Scaffold"/>
+      </Column>
+      <Column text="In Progress">
+        <Card text="Learn Typescript"/>
+      </Column>
+      <Column text="Done">
+        <Card text="Begin Using Static Typing"/>
+      </Column>
+      <AddNewItem toggleButtonText="+ Add a new Item" onAdd={console.log}/>
+    </AppContainer>
   );
 }
 
